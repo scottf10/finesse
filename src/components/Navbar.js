4ecfad80c950c36290c76f41adeb3189974f1414
Scott Florence Jr <scott10florence@gmail.com>
2020-07-23 18:16:17 -0400

    add existing file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,11 +28,11 @@ const Navbar = class extends React.Component {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
-            })
+            navBarActiveClass: 'is-active',
+          })
           : this.setState({
-              navBarActiveClass: '',
-            })
+            navBarActiveClass: '',
+          })
       }
     )
   }
@@ -81,38 +81,31 @@ const Navbar = class extends React.Component {
                 Form Examples
               </Link>
             </div> */}
-            
+
             <div className="column is-4 social navbar-end has-text-centered">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="tiktok" href="https://vimeo.com">
-                  <img
-                    src={tiktok}
-                    alt="TikTok"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-              </div>
+              <a title="instagram" href="https://www.instagram.com/scottflo/">
+                <img
+                  src={instagram}
+                  alt="Instagram"
+                  style={{ width: '1em', height: '1em' }}
+                />
+              </a>
+              <a title="twitter" href="https://twitter.com/scottflorencejr">
+                <img
+                  className="fas fa-lg"
+                  src={twitter}
+                  alt="Twitter"
+                  style={{ width: '1em', height: '1em' }}
+                />
+              </a>
+              <a title="tiktok" href="https://www.tiktok.com/@finesseuniversity?lang=en">
+                <img
+                  src={tiktok}
+                  alt="TikTok"
+                  style={{ width: '1em', height: '1em' }}
+                />
+              </a>
+            </div>
           </div>
         </div>
       </nav>
